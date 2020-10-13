0b3ac383030c5e7b4dc5056f727801fe88d1fb69
argaitt <argaitt@gmail.com>
2020-10-13 18:00:45 +0300

    init

---
+++ CPP_Lab_2_Faker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CPP_Lab_2_Faker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
