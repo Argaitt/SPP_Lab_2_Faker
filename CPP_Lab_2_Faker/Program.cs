7079492d0e0b60c7c4ad5a26aaeae2726385f73a
argaitt <argaitt@gmail.com>
2020-10-27 23:38:53 +0300

    feat: cycle ref protected

--- CPP_Lab_2_Faker/Program.cs
+++ CPP_Lab_2_Faker/Program.cs
@@ -10,6 +10,7 @@ namespace CPP_Lab_2_Faker
         {
             Faker faker = new Faker();
             Foo foo = faker.Create<Foo>();
+            Bar bar = faker.Create<Bar>();
             
         }
     }
@@ -25,5 +26,20 @@ namespace CPP_Lab_2_Faker
         public List<string> test8;
         public double test9;
         public List<double> test10;
+        public Bar bar;
+    }
+    class Bar
+    {
+        public int test1;
+        public string test2;
+        public long test3;
+        public float test4;
+        public string test5;
+        public DateTime test6;
+        public List<int> test7;
+        public List<string> test8;
+        public double test9;
+        public List<double> test10;
+        public Foo foo;
     }
 }
