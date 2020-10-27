48f6bf47f7fe3b7bd23c3cc62663bfd2c954d968
argaitt <argaitt@gmail.com>
2020-10-27 23:34:21 +0300

    feat: complate generator logic for Faker class

--- CPP_Lab_2_Faker/Program.cs
+++ CPP_Lab_2_Faker/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Fakernamespace;
 using System;
+using System.Collections.Generic;
 
 namespace CPP_Lab_2_Faker
 {
@@ -17,5 +18,12 @@ namespace CPP_Lab_2_Faker
         public int test1;
         public string test2;
         public long test3;
+        public float test4;
+        public string test5;
+        public DateTime test6;
+        public List<int> test7;
+        public List<string> test8;
+        public double test9;
+        public List<double> test10;
     }
 }
--- Faker/Faker.cs
+++ Faker/Faker.cs
@@ -5,13 +5,47 @@ using System.Reflection;
 
 namespace Fakernamespace
 {
-    interface IFakeGenerator 
+    
+    public class Faker
     {
-        object Generate(); 
+        Dictionary<Type, FakeGenerator> FakeGeneratorDictionary;
+        public Faker() 
+        {
+            FakeGeneratorDictionary = new Dictionary<Type, FakeGenerator>();
+            FakeGeneratorDictionary.Add(typeof(int), new IntFakeGenerator());
+            FakeGeneratorDictionary.Add(typeof(long), new LongFakeGenerator());
+            FakeGeneratorDictionary.Add(typeof(string), new StringFakeGenerator());
+            FakeGeneratorDictionary.Add(typeof(double), new DoubleFakeGenerator());
+            FakeGeneratorDictionary.Add(typeof(float), new FloatFakeGenerator());
+            FakeGeneratorDictionary.Add(typeof(DateTime), new DateTimeFakeGenerator());
+            FakeGeneratorDictionary.Add(typeof(List<int>), new ArrIntFakeGenerator());
+            FakeGeneratorDictionary.Add(typeof(List<string>), new ArrStringFakeGenerator());
+            FakeGeneratorDictionary.Add(typeof(List<double>), new ArrDoubleFakeGenerator());
+        }
+        public T Create<T>()
+        {
+            var obj = (T)Activator.CreateInstance(typeof(T));
+            var type = obj.GetType();
+            FieldInfo[] myFields = type.GetFields();
+            foreach (var item in myFields)
+            {
+                if (FakeGeneratorDictionary.TryGetValue(item.FieldType, out var generator))
+                    item.SetValue(obj, generator.Generate());
+                else 
+                {
+                    item.SetValue(obj, null);
+                }
+            }
+            return obj;
+        }
+    }
+    interface IFakeGenerator
+    {
+        object Generate();
     }
     class FakeGenerator : IFakeGenerator
     {
-        public virtual object Generate() 
+        public virtual object Generate()
         {
             object obj = 0;
             return obj;
@@ -24,27 +58,60 @@ namespace Fakernamespace
             return (int)12;
         }
     }
-    public class Faker
+    class LongFakeGenerator : FakeGenerator
     {
-        Dictionary<Type, FakeGenerator> FakeGeneratorDictionary;
-        public Faker() 
+        public override object Generate()
         {
-            FakeGeneratorDictionary = new Dictionary<Type, FakeGenerator>();
-            FakeGeneratorDictionary.Add(typeof(int), new IntFakeGenerator());
+            return (long)12;
         }
-        public T Create<T>()
+    }
+    class StringFakeGenerator : FakeGenerator
+    {
+        public override object Generate()
         {
-            var obj = (T)Activator.CreateInstance(typeof(T));
-            var type = obj.GetType();
-            FieldInfo[] myFields = type.GetFields();
-            
-            foreach (var item in myFields)
-            {
-                FakeGenerator generator = new FakeGenerator();
-                FakeGeneratorDictionary.TryGetValue(item.FieldType, out generator);
-                item.SetValue(obj, generator.Generate());
-            }
-            return obj;
+            return "string";
+        }
+    }
+    class FloatFakeGenerator : FakeGenerator
+    {
+        public override object Generate()
+        {
+            return (float)23.1;
+        }
+    }
+    class DateTimeFakeGenerator : FakeGenerator
+    {
+        public override object Generate()
+        {
+            return DateTime.Now;
+        }
+    }
+    class DoubleFakeGenerator : FakeGenerator
+    {
+        public override object Generate()
+        {
+            return (double)000.1;
+        }
+    }
+    class ArrIntFakeGenerator : FakeGenerator
+    {
+        public override object Generate()
+        {
+            return new List<int> { 1, 2, 3, 4, 5, 6 };
+        }
+    }
+    class ArrDoubleFakeGenerator : FakeGenerator
+    {
+        public override object Generate()
+        {
+            return new List<double> {1.0, 2.0, 3.0, 4.0, 5.0, 6.0};
+        }
+    }
+    class ArrStringFakeGenerator : FakeGenerator
+    {
+        public override object Generate()
+        {
+            return new List<string> { "1","2" ,"3","4","5","6"};
         }
     }
 }
