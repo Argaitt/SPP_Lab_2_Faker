ba3e7c97c21a46ed358813c46b011d7919f5b531
argaitt <argaitt@gmail.com>
2020-10-27 20:11:58 +0300

    add: default generators for faker

--- Faker/Faker.cs
+++ Faker/Faker.cs
@@ -1,11 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Dynamic;
 using System.Reflection;
 
 namespace Fakernamespace
 {
+    interface IFakeGenerator 
+    {
+        object Generate(); 
+    }
+    class FakeGenerator : IFakeGenerator
+    {
+        public virtual object Generate() 
+        {
+            object obj = 0;
+            return obj;
+        }
+    }
+    class IntFakeGenerator : FakeGenerator
+    {
+        public override object Generate()
+        {
+            return (int)12;
+        }
+    }
     public class Faker
     {
+        Dictionary<Type, FakeGenerator> FakeGeneratorDictionary;
+        public Faker() 
+        {
+            FakeGeneratorDictionary = new Dictionary<Type, FakeGenerator>();
+            FakeGeneratorDictionary.Add(typeof(int), new IntFakeGenerator());
+        }
         public T Create<T>()
         {
             var obj = (T)Activator.CreateInstance(typeof(T));
@@ -14,7 +40,9 @@ namespace Fakernamespace
             
             foreach (var item in myFields)
             {
-                item.SetValue(obj, 1);
+                FakeGenerator generator = new FakeGenerator();
+                FakeGeneratorDictionary.TryGetValue(item.FieldType, out generator);
+                item.SetValue(obj, generator.Generate());
             }
             return obj;
         }
